21fc0e9e328d57d28d8c8a7a083b81f5d0cc9dbe
crwgregory <crwgregory@gmail.com>
2017-12-26 19:48:34 -0700

    update: instructions

--- index.php
+++ index.php
@@ -7,3 +7,4 @@ $mysqlConnection = null; // todo: create mysqli connection if you want to test
 $database = new \app\QueryLocalDBRequest($tables, $mysqlConnection);
 
 // todo: follow instructions in README.md
+// when you are done, submit your code by zipping the project and sending it in an email as an attachment
