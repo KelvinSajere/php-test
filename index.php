dcb8a0de6817cc8146621ce069e4d0b8d20789a2
kelvin sajere <yovwisajere@gmail.com>
2018-02-23 21:56:18 -0700

    Added Database Connection
    Added Error Logging Features

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
-
+require_once __DIR__ . '/vendor/autoload.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 ini_set("log_errors", 1);
@@ -27,8 +26,41 @@ $where   = array(
 $orderBy = array(
     'linkID'
 );
-$mysqlConnection = null; // todo: create mysqli connection if you want to test
 
-$database = new \app\QueryLocalDBRequest($tables, $mysqlConnection);
+try {
+    $mysqlConnection = mysqli_connect("localhost", "root", "", "marrick"); // todo: create mysqli connection if you want to test
+}
+catch (\Exception $e) {
+
+    $errorCode = uniqid();
+    error_log(sprintf('[%s] %s error connecting to  database::: %s', __FILE__, 'index.php', $e->getCode()));
+    echo "Error connecting to database";
+    die();
+
+
+}
+
+try {
+    $database = new \app\QueryLocalDBRequest($tables, $mysqlConnection);
+}
+catch (\Exception $e) {
+
+    $errorCode = uniqid();
+    error_log(sprintf('[%s] %s error setting query   ::: %s', __FILE__, $database->error, $e->getCode()));
+    echo "Error connecting to database";
+    die();
+}
+try {
+    $result = $database->get($select, $where, $orderBy, null);
+}
+catch (\Exception $e) {
+
+    $errorCode = uniqid();
+    error_log(sprintf('[%s] %s :: error in QueryLocalDBRequest.php   ::: %s', __FILE__, $database->error, $e->getCode()));
+    echo "Error Running Query";
+    die();
+}
+//display result in json format
+echo json_encode($result, \JSON_PRETTY_PRINT | \JSON_FORCE_OBJECT | \JSON_NUMERIC_CHECK | \JSON_PRESERVE_ZERO_FRACTION);
 
 // todo: follow instructions in README.md
